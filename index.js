960330d321b9f8f153212252301aa3e637134e31
Nix-WX <55787274+Nix-WX@users.noreply.github.com>
2020-09-07 15:04:53 +0800

    deleted key and cert file and removed related code

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const http = require('http');
 const app = require('./app');
 
-const https = require('https');
-const fs = require('fs');
-
-const options = {
-    key: fs.readFileSync('key.pem'),
-    cert: fs.readFileSync('cert.pem')
-};
-
 const port = process.env.PORT;
 const server = http.createServer(app);
 
